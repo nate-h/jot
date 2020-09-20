58ce8836cb654f51877bc3f12ad2fa0800c05f81
nates linux desktop <nhapeman@gmail.com>
2020-09-20 13:11:01 -0700

    First pass at creating a better note taking app.

---
+++ run_this_once.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+> settings.cfg
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+# Get users favorite editor and write to settings cfg.
+read -p "Favorite terminal text editor [micro]: " editor
+JOT_EDITOR=${name:-micro}
+echo "Specified editor: $JOT_EDITOR"
+echo "JOT_EDITOR=$JOT_EDITOR" >> settings.cfg
+
+# Specify where to save the notes.
+DEF_REPO=git@github.com:nate-h/jot_notes.git
+read -p "Your private notes editor repo [$DEF_REPO]: " repo
+NOTES_REPO=${repo:-$DEF_REPO}
+echo "Specified notes repo: $NOTES_REPO"
+echo "NOTES_REPO=$NOTES_REPO" >> settings.cfg
+
+# Initialize submodule.
+if [ ! -d "$DIR/jot_notes" ]; then
+    echo "Initializing submodule..."
+    git clone $NOTES_REPO jot_notes
+else
+    echo "Submodule already exists. Not recreating."
+fi
